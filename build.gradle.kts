00ad435a2ddf5a26b055ac02943e6fa85687eec4
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-11-11 18:19:42 +0100

    chore(ci): remove unnecessary gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(libs.plugins.dokka)
     alias(libs.plugins.gitSemVer)
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.kotlin.qa)
-    alias(libs.plugins.publishOnCentral)
-    alias(libs.plugins.multiJvmTesting)
-    alias(libs.plugins.taskTree)
 }
 
 group = "it.unibo.spe.compose"
@@ -40,35 +35,5 @@ tasks.test {
         showStackTraces = true
         events(*org.gradle.api.tasks.testing.logging.TestLogEvent.values())
         exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-    }
-}
-
-signing {
-    val signingKey: String? by project
-    val signingPassword: String? by project
-    useInMemoryPgpKeys(signingKey, signingPassword)
-}
-
-publishOnCentral {
-    projectLongName.set("Template Kotlin JVM Project")
-    projectDescription.set("A template repository for Kotlin JVM projects")
-    repository("https://maven.pkg.github.com/danysk/${rootProject.name}".lowercase()) {
-        user.set("DanySK")
-        password.set(System.getenv("GITHUB_TOKEN"))
-    }
-    publishing {
-        publications {
-            withType<MavenPublication> {
-                pom {
-                    developers {
-                        developer {
-                            name.set("Danilo Pianini")
-                            email.set("danilo.pianini@gmail.com")
-                            url.set("http://www.danilopianini.org/")
-                        }
-                    }
-                }
-            }
-        }
     }
 }
