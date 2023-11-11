5169d2c7c23d3a2056332f43c7690970e3948ad0
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-11-11 18:08:41 +0100

    test: use docker compose for testing on MariaDB

--- src/test/kotlin/it/unibo/spe/compose/TestMariaDBCustomerRepository.kt
+++ src/test/kotlin/it/unibo/spe/compose/TestMariaDBCustomerRepository.kt
@@ -6,9 +6,13 @@ import it.unibo.spe.compose.impl.SqlCustomerRepository
 import org.junit.AfterClass
 import org.junit.Before
 import org.junit.BeforeClass
+import java.io.File
+import java.lang.IllegalArgumentException
 import java.time.LocalDate
+import java.util.UUID
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class TestMariaDBCustomerRepository {
 
@@ -20,21 +24,63 @@ class TestMariaDBCustomerRepository {
         const val PASSWORD = "password"
         const val TABLE = "customers"
 
+        private lateinit var composeFile: File
+
+        private fun createComposeFile(path: File, vararg assignments: Pair<String, String>): File {
+            val variablesAssignments = assignments.toMap()
+            TestMariaDBCustomerRepository::class.java.getResource("docker-compose.yml")
+                ?.openStream()?.bufferedReader()?.use { reader ->
+                    path.bufferedWriter().use { writer ->
+                        reader.forEachLine { line ->
+                            val newLine = variablesAssignments.entries.fold(line) { acc, (variable, value) ->
+                                acc.replace("__${variable}__", value)
+                            }
+                            writer.write(newLine)
+                            writer.newLine()
+                        }
+                    }
+                } ?: error("docker-compose.yaml resource not found")
+            return path
+        }
+
+        private fun executeDockerCompose(vararg arguments: String, async: Boolean = false): Process {
+            val command = mutableListOf("docker", "compose", "-f", composeFile.absolutePath)
+            command.addAll(arguments)
+            val commandString = command.joinToString(" ")
+            return ProcessBuilder(command).inheritIO().start().also {
+                if (!async) {
+                    it.waitFor()
+                    assertEquals(
+                        expected = 0,
+                        actual = it.exitValue(),
+                        message = "Command `$commandString` returned non-zero exit code: ${it.exitValue()}",
+                    )
+                }
+            }
+        }
+
         @BeforeClass
         @JvmStatic
         fun setUpMariaDb() {
-            // TODO: copy the "docker-compose.yml" resource into a temporary directory D
-            // TODO: replace variables in __THIS_FORM__ in the copied file, to match the constants above
-            // TODO: use docker compose command programmatically to start one new fresh MariaDB instance
-            // TODO: wait for the MariaDB instance to be up and running
-            TODO("implement me")
+            composeFile = createComposeFile(
+                path = File.createTempFile("docker-compose", ".yml"),
+                "VERSION" to "latest",
+                "ROOT_PASSWORD" to UUID.randomUUID().toString(),
+                "DB_NAME" to DATABASE,
+                "USER" to USER,
+                "PASSWORD" to PASSWORD,
+                "PORT" to "$PORT",
+            )
+
+            executeDockerCompose("up", "--wait")
+            executeDockerCompose("logs", "db", "--tail", "1")
         }
 
         @AfterClass
         @JvmStatic
         fun tearDownMariaDb() {
-            // TODO: use docker compose command programmatically to stop the currently running MariaDB instance
-            TODO("implement me")
+            executeDockerCompose("logs", "db")
+            executeDockerCompose("down", "--volumes")
         }
     }
 
@@ -54,6 +100,11 @@ class TestMariaDBCustomerRepository {
         "Giovanni",
         "Ciatto",
         LocalDate.of(1992, 4, 7),
+    )
+
+    private val person2 = person.clone(
+        birthDate = LocalDate.of(1992, 4, 8),
+        id = TaxCode("CTTGNN92D08D468M"),
     )
 
     private val vatNumber = VatNumber(12345678987L)
@@ -85,8 +136,11 @@ class TestMariaDBCustomerRepository {
             expected = listOf(company),
             actual = repository.findById(vatNumber),
         )
-        repository.remove(taxCode)
         repository.remove(vatNumber)
+        repository.update(taxCode, person2)
+        assertFailsWith<IllegalArgumentException>("Invalid customer id:") {
+            repository.remove(taxCode)
+        }
         assertEquals(
             expected = emptyList(),
             actual = repository.findById(taxCode),
@@ -95,5 +149,15 @@ class TestMariaDBCustomerRepository {
             expected = emptyList(),
             actual = repository.findById(vatNumber),
         )
+        assertEquals(
+            expected = listOf(person2),
+            actual = repository.findById(person2.id),
+        )
+        listOf("Giovanni", "Ciatto").forEach {
+            assertEquals(
+                expected = listOf(person2),
+                actual = repository.findByName(it),
+            )
+        }
     }
 }
